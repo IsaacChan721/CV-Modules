f0a8c9a3e64c9029c5e87b8992c7689f21115cdc
isaacchan721 <isaacchan721@gmail.com>
2024-07-18 17:50:29 -0400

    minor adjustments

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -3,14 +3,15 @@ import mediapipe as mp
 import time
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2, model_complexity = 1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode # depends on if its an image or video
         self.maxHands = maxHands
+        self.model_complexity = model_complexity
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.model_complexity, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
         
     def findHands(self, img, draw=True):
@@ -18,28 +19,20 @@ class handDetector():
         self.results = self.hands.process(imgRGB)
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
-                # for drawing circles on certain landmarks of the hand
-                # for id, lm in enumerate(handLms.landmark):
-                #     h, w, _ = img.shape
-                #     cx, cy = int(lm.x*w), int(lm.y*h)
-                #     if id == 0: # particular landmark
-                #         cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
     
-    def main():
-        cap = cv2.VideoCapture(0)
-        detector = handDetector()
-        FPS = FPS()
-        while True:
-            ret, img = cap.read()
-            if ret:
-                img = detector.findHands(img)
-                cv2.putText(img, f"FPS: {str(FPS.update())}", (0,0), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 3)
-                cv2.imshow("Image", img)
-                cv2.waitKey(1)
-                
+    def findPosition(self, img, handNo=0, draw=True):
+        lm_pos = []
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                # for drawing circles on certain landmarks of the hand
+                for id, lm in enumerate(handLms.landmark):
+                    h, w, _ = img.shape
+                    cx, cy = int(lm.x*w), int(lm.y*h)
+                    lm_pos.append([id, cx, cy])
+        return lm_pos        
 class FPS():
     def __init__(self):
         self.start = time.time()
@@ -53,3 +46,18 @@ class FPS():
             self.frames = 0
             self.start = time.time()
         return self.fps
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    counter = FPS()
+    while True:
+        ret, img = cap.read()
+        img = detector.findHands(img)
+        print(detector.findPosition(img))
+        cv2.putText(img, f"FPS: {str(counter.update())}", (50,50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1) # 1 = video, 0 = image
+
+if __name__ == "__main__": 
+    main()
